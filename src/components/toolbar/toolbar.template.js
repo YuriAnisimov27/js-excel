8382bce804e773df26ddc588e43a720f3f65e8f3
Yuri Anisimov <yurianisimov27@gmail.com>
2020-12-04 18:05:28 +0300

    feat: toggle icon bold

--- src/components/toolbar/toolbar.template.js
+++ src/components/toolbar/toolbar.template.js
@@ -14,7 +14,7 @@ function toButton(button) {
   `;
 }
 
-export function createToolbar() {
+export function createToolbar(state) {
   const buttons = [
     {
       icon: 'format_align_left',
@@ -23,7 +23,7 @@ export function createToolbar() {
     },
     {
       icon: 'format_align_center',
-      active: true,
+      active: false,
       value: {textAlign: 'center'},
     },
     {
@@ -33,8 +33,8 @@ export function createToolbar() {
     },
     {
       icon: 'format_bold',
-      active: true,
-      value: {fontWeight: 'bold'},
+      active: state['fontWeight'] === 'bold',
+      value: {fontWeight: state['fontWeight'] === 'bold' ? 'normal' : 'bold'},
     },
     {
       icon: 'format_italic',
