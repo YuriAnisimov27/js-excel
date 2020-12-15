94ac2d649ac6e23e1b05d083a1a61c1d96c53402
YuriAnisimov27 <yurianisimov27@gmail.com>
2020-12-15 20:55:32 +0300

    feat: implement page change

--- src/core/routes/Router.js
+++ src/core/routes/Router.js
@@ -1,4 +1,5 @@
 import {$} from '@core/dom';
+import {ActiveRoute} from '@core/routes/ActiveRoute';
 
 export default class Router {
   constructor(selector, routes) {
@@ -8,6 +9,8 @@ export default class Router {
 
     this.$placeholder = $(selector);
     this.routes = routes;
+
+    this.page = null;
 
     this.changePageHandler = this.changePageHandler.bind(this);
 
@@ -20,11 +23,19 @@ export default class Router {
   }
 
   changePageHandler() {
-    const Page = this.routes.excel;
-    const page = new Page;
-    this.$placeholder.append(page.getRoot());
+    if (this.page) {
+      this.page.destroy();
+    }
 
-    page.afterRender();
+    this.$placeholder.clear();
+    const Page = ActiveRoute.path.includes('excel')
+      ? this.routes.excel
+      : this.routes.dashboard;
+
+    this.page = new Page(ActiveRoute.param);
+    this.$placeholder.append(this.page.getRoot());
+
+    this.page.afterRender();
   }
 
   destroy() {
